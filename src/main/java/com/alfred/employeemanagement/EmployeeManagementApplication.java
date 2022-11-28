0204710df2530308887dba3e024768ef1def977c
alfredcargar <alfredcargar@outlook.it>
2022-11-28 11:06:18 +0100

    cleanup

--- src/main/java/com/alfred/employeemanagement/EmployeeManagementApplication.java
+++ src/main/java/com/alfred/employeemanagement/EmployeeManagementApplication.java
@@ -2,7 +2,6 @@ package com.alfred.employeemanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class EmployeeManagementApplication {
@@ -10,11 +9,5 @@ public class EmployeeManagementApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeManagementApplication.class, args);
     }
-
-
-//    @Override
-//    public void run(String... args) throws Exception {
-//
-//    }
 
 }
